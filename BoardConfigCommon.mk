bfefbde64285b07042fc528b9e4cfdc888375ea7
Rakhshan Ali <alirakhshan7070@gmail.com>
2024-12-21 03:35:34 +0530

    sm7325-common: Use Dolby Atmos from hardware/motorola
    Signed-off-by: Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -110,11 +110,11 @@ LOC_HIDL_VERSION := 4.2
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 ifeq ($(TARGET_HAS_SNXXX_NFC),)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_nfc.xml
