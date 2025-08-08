2dc848f7ee5eadb8eff3efda40bc4566c75114f4
Vishalcj17 <vishalcj@aospa.co>
2025-08-08 11:33:32 +0000

    sm7325-common: Disable per-cgroup PSI accounting
    * Per-cgroup PSI accounting is not used in Android while having small performance cost. Disable this unused feature.
    
    Bug: 233410456
    
    Change-Id: I6ca0c9b5b317e29ce7e38742fd63ecf7402a7ee4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,7 +30,7 @@ BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3
-BOARD_KERNEL_CMDLINE += swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket
+BOARD_KERNEL_CMDLINE += swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket cgroup_disable=pressure
 BOARD_KERNEL_CMDLINE += pcie_ports=compat iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
