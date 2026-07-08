6a98c3936ae5f9a0502b532c3d1c12915539e989
Adithya R <gh0strider.2k18.reborn@gmail.com>
2026-07-08 23:36:21 +0530

    sm7325-common: parts: Override HDR types for dolby vision
    * [Dheeraj3031A]: Introduce MotoParts
    
    Stock display HAL doesn't enable dolby vision support,
    instead stock uses a custom surfaceflinger lib for the
    same. Since we won't be using that, simply override
    supported HDR types at boot.
    
    Change-Id: I93ecfbd7aef42e5e1a4c696e196e482a023c9ae4

--- common.mk
+++ common.mk
@@ -227,6 +227,10 @@ PRODUCT_PACKAGES += \
     MotoActions \
     MotoCommonOverlay
 
+# Moto Parts
+PRODUCT_PACKAGES += \
+    MotoParts
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
